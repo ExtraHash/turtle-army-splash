42f793a926f7e426d18d6d2ea8231b7ccbf0a289
ExtraHash <ExtraHash@protonmail.com>
2020-04-28 17:32:42 -0600

    make a cool splashscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logoWhite from "./logo-white.svg";
+import logoGreen from './logo-green.svg';
+
+export let darkMode = false;
+
+if (window.matchMedia("(prefers-color-scheme)").media !== "not all") {
+  console.log("🎉 prefers-color-scheme is supported");
+  darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  console.log("Dark mode OS preference is " + darkMode);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="root">
+      <header className="header">
+        <img src={darkMode ? logoWhite : logoGreen} className="logo" alt="logo" />
+        <h1 className="title is-family-monospace">turtle.army</h1>
+        <a className="app-link" href="https://explorer.turtle.army">
+          explorer
         </a>
       </header>
     </div>
